f399d0d39feb7d461f0a9147b34d45281e3ca67b
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-01-31 15:46:28 -0500

    Create company.js

---
+++ models/company.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  address: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Company", companySchema);
