8f3b17c0c9e7c486f6e6e16f70a7f535e67319a9
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-02-01 01:59:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 // ----- Middleware (must come BEFORE routes) -----
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"] }));
+
+
 // ----- Swagger Setup -----
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
