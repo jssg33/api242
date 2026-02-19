aa58b12713dba5c5b518cd0eca99ab231da090e7
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-02-18 20:15:45 -0500

    Update LicenseType.js

--- models/LicenseType.js
+++ models/LicenseType.js
@@ -21,18 +21,5 @@ const LicenseTypeSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Auto-increment LicenseTypeID before saving
-LicenseTypeSchema.pre('save', async function (next) {
-  if (this.isNew) {
-    const counter = await Counter.findByIdAndUpdate(
-      { _id: 'LicenseTypeID' },
-      { $inc: { seq: 1 } },
-      { new: true, upsert: true }
-    );
-
-    this.LicenseTypeID = counter.seq;
-  }
-  next();
-});
 
 module.exports = mongoose.model('LicenseType', LicenseTypeSchema);
