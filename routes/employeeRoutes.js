c9c5234f0289bff91fa6bf4b56dd17bc4f7981e3
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-03-22 23:05:37 -0400

    Create employeeRoutes.js

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,115 @@
+import express from "express";
+import {
+  getEmployees,
+  getEmployeeById,
+  createEmployee,
+  updateEmployee,
+  deleteEmployee
+} from "../controllers/employeeController.js";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Employees
+ *   description: Employee management API
+ */
+
+/**
+ * @swagger
+ * /api/employees:
+ *   get:
+ *     summary: Get all employees
+ *     tags: [Employees]
+ *     responses:
+ *       200:
+ *         description: List of employees
+ */
+router.get("/", getEmployees);
+
+/**
+ * @swagger
+ * /api/employees/{id}:
+ *   get:
+ *     summary: Get employee by ID
+ *     tags: [Employees]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Employee data
+ *       404:
+ *         description: Employee not found
+ */
+router.get("/:id", getEmployeeById);
+
+/**
+ * @swagger
+ * /api/employees:
+ *   post:
+ *     summary: Create a new employee
+ *     tags: [Employees]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Employee'
+ *     responses:
+ *       201:
+ *         description: Employee created
+ */
+router.post("/", createEmployee);
+
+/**
+ * @swagger
+ * /api/employees/{id}:
+ *   put:
+ *     summary: Update an employee
+ *     tags: [Employees]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Employee'
+ *     responses:
+ *       200:
+ *         description: Employee updated
+ *       404:
+ *         description: Employee not found
+ */
+router.put("/:id", updateEmployee);
+
+/**
+ * @swagger
+ * /api/employees/{id}:
+ *   delete:
+ *     summary: Delete an employee
+ *     tags: [Employees]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Employee deleted
+ *       404:
+ *         description: Employee not found
+ */
+router.delete("/:id", deleteEmployee);
+
+export default router;
