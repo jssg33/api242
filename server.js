c2c5e13fa70b51e8ec4d1d4b47636f3fab7456cc
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-03-24 21:36:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.use(
   cors({
     origin: ["*", "https://jssg33.github.io", "https://react242-ho2o.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 
 );
