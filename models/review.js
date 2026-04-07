a014ae37e3c06c7fbb506cb331d3765878b63a37
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-04-07 17:37:01 -0400

    Update review.js

--- models/review.js
+++ models/review.js
@@ -2,21 +2,49 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
   {
-    // Unified author field (used for CGPARKS + product reviews)
+
+    // Unified author field (used for CGPARKS + product reviews) MongoDB ID
     uid: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false
+    },
+    
+    // Unified author field (used for CGPARKS + product reviews)
+    uidGUID: {
       type: String,
-      required: true
+      required: false
     },
 
-    // Optional: product review linkage
+    // Optional: product review linkage for MongoDB ID
     productId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Product"
+      ref: "Product",
+      required: false
+    },
+    // Optional: product review linkage for Parks GUIDS or Other Product String
+    productGUID: {
+      type: String,
+      ref: "ProductGUID",
+      required: false
     },
 
-    // Optional: park review linkage
+    // Optional: park review linkage MongoDB ID
     parkId: {
-      type: Number
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ParkMongo",
+      required: false
+    },
+    // Optional: park review linkage ParkGUID or Other Product String
+    parkGUID: {
+      type: String,
+      ref: "ParkGUID",
+      required: false
+    },
+    // Optional: park review linkage Legacy Parks
+    parkLEGACY: {
+      type: String,
+      ref: "ParkLegacyCSharp",
+      required: false
     },
 
     // Rating 1–5
