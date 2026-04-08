44b129278c932dff9eac3b0b82ac426cfabcf878
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-04-08 10:38:23 -0400

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -24,7 +24,7 @@
  *         username:
  *           type: string
  *           example: johndoe
- *         plainPassword:
+ *         plainpassword:
  *           type: string
  *           example: StrongPassword123
  *
