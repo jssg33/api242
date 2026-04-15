b9156da20bbe0d4a62f6e4d44a2b267b04cd32a8
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-04-15 18:17:50 -0400

    Update userlog.js

--- models/userlog.js
+++ models/userlog.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const UserLogSchema = new mongoose.Schema({
   id: { type: Number, required: true },
   username: { type: String, required: true },
-  hashid: { type: Number, required: true },
+  hashid: { type: Number, required: false },
+  mongoid: { type: String, required: false },
   location: { type: String, required: false },
-  loginstatus: { type: String, required: true },
+  loginstatus: { type: String, required: false },
   ipaddress: { type: String, required: false },
-  description: { type: String },
-  uiorigin: { type: String }
+  description: { type: String, required: true },
+  uiorigin: { type: String, required: false },
+  instance: { type: String, required: false },
+  region: { type: String, required: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('UserLog', UserLogSchema);
