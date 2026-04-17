0448b9e3a7511ec132adb84c2c89df431d727078
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-04-16 21:46:57 -0400

    Update adminLogController.js

--- controllers/adminLogController.js
+++ controllers/adminLogController.js
@@ -1,5 +1,6 @@
 const AdminLog = require('../models/AdminLog');
 
+// GET all logs
 exports.getAllAdminLogs = async (req, res) => {
   try {
     const logs = await AdminLog.find();
@@ -9,6 +10,7 @@ exports.getAllAdminLogs = async (req, res) => {
   }
 };
 
+// GET log by numeric id (existing behavior)
 exports.getAdminLogById = async (req, res) => {
   try {
     const log = await AdminLog.findOne({ id: req.params.id });
@@ -21,6 +23,20 @@ exports.getAdminLogById = async (req, res) => {
   }
 };
 
+// NEW: GET log by mongoid
+exports.getAdminLogByMongoId = async (req, res) => {
+  try {
+    const log = await AdminLog.findOne({ mongoid: req.params.mongoid });
+    if (!log) {
+      return res.status(404).json({ message: 'Admin log not found' });
+    }
+    res.json(log);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// POST create new log (existing)
 exports.createAdminLog = async (req, res) => {
   try {
     const log = new AdminLog(req.body);
@@ -30,3 +46,23 @@ exports.createAdminLog = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+// NEW: PUT update log by mongoid
+exports.updateAdminLogByMongoId = async (req, res) => {
+  try {
+    const updated = await AdminLog.findOneAndUpdate(
+      { mongoid: req.params.mongoid },
+      req.body,
+      { new: true }
+    );
+
+    if (!updated) {
+      return res.status(404).json({ message: 'Admin log not found' });
+    }
+
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
