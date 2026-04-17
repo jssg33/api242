f483f8cdaf605ee8a8b1be598655c79addae1f93
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-04-17 12:42:34 -0400

    Update licenselog.js

--- models/licenselog.js
+++ models/licenselog.js
@@ -4,12 +4,13 @@ const licenseLogSchema = new mongoose.Schema(
   {
     licenseid: { type: String, required: true, trim: true },
     accessdate: { type: String, required: true },
-    userid: { type: String, required: true, trim: true },
+    userid: { type: String, required: false, trim: true },
     managerid: { type: String, required: false, trim: true },
     groupid: { type: String, required: false, trim: true },
+    mongoid: { type: String, required: false, trim: true },
     companyid: { type: String, required: false, trim: true },
-    shard: { type: String, required: true },
-    instanceid: { type: String, required: true },
+    shard: { type: String, required: false },
+    instanceid: { type: String, required: false },
     licensestatus: {
       type: String,
       required: true,
