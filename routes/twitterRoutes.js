a812d6c9504056fb8b80bfc0016db0f0e28f1faf
jssg33 <john@glocation.info>
2026-05-24 12:14:55 -0400

    Fix import path for TwitterRequest model

--- routes/twitterRoutes.js
+++ routes/twitterRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const validateRequest = require('../middleware/validateRequest');
-const { twitterRequestSchema } = require('../models/twitterRequestModel');
+const { twitterRequestSchema } = require('../models/TwitterRequest');
 const { createTwitterRequest } = require('../controllers/twitterRequestController');
 
 /**
