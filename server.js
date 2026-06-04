fded0170451173e5127bb79f0c90460a19b40334
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2026-06-04 00:38:20 -0400

    Fix IP range comment for CTS in server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const uscIpRange = /^129\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
 // AT&T: 99.0.0.0/8
 const attIpRange = /^99\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
 
-// CTS: 107.0.0.0/24 VIA ATT ASSIGNED RANGE
+// CTS: 107.0.0.0/8 VIA ATT ASSIGNED RANGE
 const attIpRange = /^107\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
 
 app.use(
