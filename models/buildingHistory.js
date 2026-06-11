85bcf071a9cab11506f8fd2e261a38413ff406cc
jssg33 <john@glocation.info>
2026-06-11 15:55:19 -0400

    Update buildingHistory.js

--- models/buildingHistory.js
+++ models/buildingHistory.js
@@ -3,15 +3,23 @@ const mongoose = require('mongoose');
 const BuildingHistorySchema = new mongoose.Schema({
     buildingId: { 
         type: Number, 
-        required: true 
+        required: false 
     },
     buildingName: { 
         type: String, 
+        required: false 
+    },
+    userId: { 
+        type: String, 
         required: true 
+    },
+    userName: { 
+        type: String, 
+        required: false 
     },
     timestamp: { 
         type: Number, // Stores Unix timestamp as Long
-        required: true 
+        required: false 
     },
     latitude: { 
         type: Number, 
