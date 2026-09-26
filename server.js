cacbcabc5c721a057ae1d9cdacacaacb765ff133
jssg33 <john@glocation.info>
2026-09-25 23:16:55 -0400

    Add Swagger JSON endpoint for API documentation

--- server.js
+++ server.js
@@ -113,6 +113,7 @@ app.use(
     }
   })
 );
+app.get("/swagger/v1/swagger.json", (req, res) => {res.setHeader("Content-Type", "application/json");res.send(swaggerSpec);});
 // -------------------------
 // ROUTES
 // -------------------------
